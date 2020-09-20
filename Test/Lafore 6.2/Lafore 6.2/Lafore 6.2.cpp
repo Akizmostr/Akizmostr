148692c0df256e57f5d9697477473851914304b6
Akizmostr <59568233+Akizmostr@users.noreply.github.com>
2020-09-20 21:26:24 +0300

    Add files via upload

---
+++ Test/Lafore 6.2/Lafore 6.2/Lafore 6.2.cpp
@@ -0,0 +1,73 @@
+﻿#include <iostream>
+
+const double TOLL = 0.5;
+
+class tollBooth {
+public:
+	tollBooth();
+	~tollBooth();
+	void paingCar();
+	void nopayCar();
+	const void display();
+private:
+	unsigned int cars_passed;
+	double earning;
+};
+
+tollBooth::tollBooth(): cars_passed(0), earning(0) {
+}
+
+tollBooth::~tollBooth() {
+}
+
+void tollBooth::paingCar() {
+	cars_passed++;
+	earning += TOLL;
+}
+
+void tollBooth::nopayCar() {
+	cars_passed++;
+}
+
+const void tollBooth::display() {
+	std::cout << "Cars passed: " << cars_passed << std::endl;
+	std::cout << "Total earnings: " << earning << std::endl;
+}
+
+
+int get_variant(int&, int);
+
+int main()
+{
+	std::cout << "Enter numbers for navigation: " << std::endl;
+	std::cout << "1)The driver passes with paying \n2)The driver passes without paying \n3)Display \n4)Exit" << std::endl;
+
+	int variant;
+
+	tollBooth drivers = tollBooth();
+
+	do {
+		variant = get_variant(variant, 4);
+		switch (variant) {
+		case 1:
+			drivers.paingCar();
+			break;
+		case 2:
+			drivers.nopayCar();
+			break;
+		case 3:
+			drivers.display();
+		default:
+			break;
+		}
+	} while (variant!=4);
+}
+
+int get_variant(int& variant, int n) {
+	std::cin >> variant;
+	while (variant < 1 || variant > n) {
+		std::cout << "Incorrect input. Try again." << std::endl;
+		std::cin >> variant;
+	}
+	return variant;
+}
---
+++ Test/Test/Test.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <string>
+
+class Numbers {
+public:
+	int m_num1;
+	int m_num2;
+
+	void set(int num1, int num2) {
+		m_num1 = num1;
+		m_num2 = num2;
+	}
+
+	void print() {
+		std::cout << "Numbers(" << m_num1 << "," << m_num2 << ")" << std::endl;
+	}
+};
+
+
+int main()
+{
+	Numbers n1;
+	n1.set(3, 3); // инициализируем объект n1 значениями 3 и 3
+
+	Numbers n2{ 4, 4 }; // инициализируем объект n2 значениями 4 и 4
+
+	n1.print();
+	n2.print();
+
+	return 0;
+}
